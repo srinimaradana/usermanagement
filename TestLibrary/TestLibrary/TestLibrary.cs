fe7801c47292b49a748e2f47079483b3699e1992
srinimaradana <srinivas_maradana@ryder.com>
2016-04-25 17:25:00 -0400

    Srini Commits

---
+++ TestLibrary/TestLibrary/TestLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TestLibrary
+{
+    public class TestLibrary : ContentPage
+    {
+        public TestLibrary()
+        {
+            var button = new Button
+            {
+                Text = "Click Me Now!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Clicked += (s, e) => button.Text = "Clicked by Srini: " + clicked++;
+
+            Content = button;
+        }
+    }
+}
